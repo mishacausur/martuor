2107b8fe8fe476b3bbc6ee78633e3bfa0a425225
Misha Causur <mishacausur@mail.ru>
2023-01-07 22:02:07 +0500

    types added

--- project.hs
+++ project.hs
@@ -11,3 +11,19 @@ newtype Money = Money
 newtype ProjectId = ProjectId
 { unProjectId :: Int
 } deriving (Show, Eq, Num)
+
+data Project
+	= Project ProjectId Text
+	| ProjectGroup Text [Project]
+	deriving (Show, Eq)
+
+data Budget = Budget
+{ budgetIncome :: Money
+. budgetExpenditure :: Money
+} deriving (Show, Eq)
+
+data Transaction
+	= Sale Money
+	| Purchase Money
+	deriving (Show, Eq)
+
